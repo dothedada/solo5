23b0ea07a9db9ae1471be3e6d41c395dddc6c480
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-03-02 13:13:04 -0500

    Sets the regex for spanish

--- app/taskParser.py
+++ app/taskParser.py
@@ -1,60 +1,68 @@
-from enum import Enum
 import re
 
 
-class Markers(Enum):
-    PROJECT = "@"
-    DIFICULTY = "!"
-    TASK_SEPARATOR = "//"
-
-
-class Esp_strings(Enum):
-    WEEK = "|".join(
-        (
-            "lunes",
-            "martes",
-            "mi[eé]rcoles",
-            "jueves",
-            "viernes",
-            "s[aá]bado",
-            "domingo",
-        )
-    )
-    MONTH = "|".join(
-        (
-            "enero",
-            "febrero",
-            "marzo",
-            "abril",
-            "mayo",
-            "junio",
-            "julio",
-            "agosto",
-            "septiembre",
-            "octubre",
-            "noviembre",
-            "diciembre",
-        )
-    )
-    DATE_1 = rf"([0-9]{{1,2}})(\/[0-9]{{1,2}}| de ({MONTH}))"
-    DATE_2 = rf"(de (hoy|ma[nñ]ana|este ({WEEK})) en |en )([0-9]+ )"
-    DATE_3 = r"(dentro de )([0-9]+ )(d[ií]as?|semanas?)"
-    DATE_4 = rf"(hoy|ma[nñ]ana)|(el |este |el pr[oó]ximo )({WEEK})"
-    DATE_REGX = "|".join([DATE_1, DATE_2, DATE_3, DATE_4])
+class Global_str:
+    _TASK_SEPARATOR = "//"
+    _IMPORTANT = "\\*"
+    _PROJECT = "@([a-zA-Z]+)"
+    _DIFICULTY = "!([1-5])"
 
     @classmethod
-    def get_date(cls):
-        return re.compile(cls.DATE_REGX.value, re.IGNORECASE)
+    def make_rgx(cls, *args):
+        return re.compile("|".join(args), re.IGNORECASE)
 
 
-global_regex = {
-    "project": rf"{Markers.PROJECT.value}([a-zA-Z]+)",
-    "dificulty": rf"{Markers.DIFICULTY.value}([1-5])",
-    "date_esp": Esp_strings.get_date(),
-}
+class Esp_str(Global_str):
+    __IMPORTANT_ALT = "importante"
+    __DIFICULTY_LEVELS = ("(muy )?f[aá]cil", "normal", "(muy )?dif[ií]cil")
+    __DAYS = (
+        "lunes",
+        "martes",
+        "mi[eé]rcoles",
+        "jueves",
+        "viernes",
+        "s[aá]bado",
+        "domingo",
+    )
+    __MONTH_LIST = (
+        "enero",
+        "febrero",
+        "marzo",
+        "abril",
+        "mayo",
+        "junio",
+        "julio",
+        "agosto",
+        "septiembre",
+        "octubre",
+        "noviembre",
+        "diciembre",
+    )
+    __MONTH = "|".join(__MONTH_LIST)
+    __WEEK = "|".join(__DAYS)
+    __DIFICULTY_ALT = "|".join(__DIFICULTY_LEVELS)
+    __DATE_1 = rf"([0-9]{{1,2}})(\/[0-9]{{1,2}}| de ({__MONTH}))"
+    __DATE_2 = rf"(de (hoy|ma[nñ]ana|este ({__WEEK})) en |en )([0-9]+ )"
+    __DATE_3 = r"(dentro de )([0-9]+ )(d[ií]as?|semanas?)"
+    __DATE_4 = rf"(hoy|ma[nñ]ana)|(el |este |pr[oó]ximo )({__WEEK})"
 
+    @classmethod
+    def importance_rgx(cls):
+        return super().make_rgx(cls._IMPORTANT, cls.__IMPORTANT_ALT)
 
-string = "care care @nalga : el próximo MARTES me explotaré la cabez"
-match = re.search(global_regex["date_esp"], string).group()
-match2 = re.search(global_regex["project"], string).group()
-print(match, match2)
+    @classmethod
+    def dificulty_rgx(cls):
+        return super().make_rgx(cls._DIFICULTY, cls.__DIFICULTY_ALT)
+
+    @classmethod
+    def project_rgx(cls):
+        return super().make_rgx(cls._PROJECT)
+
+    @classmethod
+    def date_rgx(cls):
+        return super().make_rgx(
+            cls.__DATE_1,
+            cls.__DATE_2,
+            cls.__DATE_3,
+            cls.__DATE_4,
+        )
