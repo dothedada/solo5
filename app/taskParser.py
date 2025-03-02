9b3583579f4466ccfb4b1da478f10b80971a6a0e
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-03-02 09:51:09 -0500

    Fixes case sensitive regex

--- app/taskParser.py
+++ app/taskParser.py
@@ -8,10 +8,20 @@ class Markers(Enum):
     TASK_SEPARATOR = "//"
 
 
-class Esp(Enum):
-    WEEK = "lunes|martes|miercoles|jueves|viernes|sabado|domingo"
+class Esp_strings(Enum):
+    WEEK = "|".join(
+        (
+            "lunes",
+            "martes",
+            "mi[eé]rcoles",
+            "jueves",
+            "viernes",
+            "s[aá]bado",
+            "domingo",
+        )
+    )
     MONTH = "|".join(
-        [
+        (
             "enero",
             "febrero",
             "marzo",
@@ -24,22 +34,27 @@ class Esp(Enum):
             "octubre",
             "noviembre",
             "diciembre",
-        ]
+        )
     )
     DATE_1 = rf"([0-9]{{1,2}})(\/[0-9]{{1,2}}| de ({MONTH}))"
     DATE_2 = rf"(de (hoy|ma[nñ]ana|este ({WEEK})) en |en )([0-9]+ )"
-    DATE_3 = r"(dentro de )([0-9]+ )(dia(?:s)?|semana(?:s)?)"
+    DATE_3 = r"(dentro de )([0-9]+ )(d[ií]as?|semanas?)"
     DATE_4 = rf"(hoy|ma[nñ]ana)|(el |este |el pr[oó]ximo )({WEEK})"
     DATE_REGX = "|".join([DATE_1, DATE_2, DATE_3, DATE_4])
+
+    @classmethod
+    def get_date(cls):
+        return re.compile(cls.DATE_REGX.value, re.IGNORECASE)
 
 
 global_regex = {
     "project": rf"{Markers.PROJECT.value}([a-zA-Z]+)",
     "dificulty": rf"{Markers.DIFICULTY.value}([1-5])",
-    "date_esp": rf"{Esp.DATE_REGX.value}",
+    "date_esp": Esp_strings.get_date(),
 }
 
 
-string = "el próximo martes me explotaré la cabez"
-match = re.match(global_regex["date_esp"], string)
-print(match)
+string = "care care @nalga : el próximo MARTES me explotaré la cabez"
+match = re.search(global_regex["date_esp"], string).group()
+match2 = re.search(global_regex["project"], string).group()
+print(match, match2)
