c21f1433734dee14ae8816ad337162ea913a850b
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-03-02 17:13:49 -0500

    Decouple lang from function and make RegexFactory

--- app/taskParser.py
+++ app/taskParser.py
@@ -1,73 +1,22 @@
 import re
-
-g_ui_and_patterns = {
-    "ui": {
-        "hi": "carajo",
-    },
-    "pattern": {
-        "task_separator": "//",
-        "important": "\\*",
-        "project": "@([a-zA-Z]+)",
-        "dificulty": "!([1-5])",
-    },
-}
-
-esp_ui_and_patterns = {
-    "ui": {
-        "hi": "hola",
-    },
-    "pattern": {
-        "important": "importante",
-        "dificulty": [
-            "muy f[aá]cil",
-            "f[aá]cil",
-            "normal",
-            "dif[ií]cil",
-            "muy dif[ií]cil",
-        ],
-        "week": [
-            "lunes",
-            "martes",
-            "mi[eé]rcoles",
-            "jueves",
-            "viernes",
-            "s[aá]bado",
-            "domingo",
-        ],
-        "months": [
-            "enero",
-            "febrero",
-            "marzo",
-            "abril",
-            "mayo",
-            "junio",
-            "julio",
-            "agosto",
-            "septiembre",
-            "octubre",
-            "noviembre",
-            "diciembre",
-        ],
-        "dates": [
-            r"([0-9]{{1,2}})(\/[0-9]{{1,2}}| de ({months}))",
-            r"(de (hoy|ma[nñ]ana|este ({week})) en |en )([0-9]+ )",
-            r"(dentro de )([0-9]+ )(d[ií]as?|semanas?)",
-            r"(hoy|ma[nñ]ana)|(el |este |pr[oó]ximo )({week})",
-        ],
-    },
-}
+from fileLoaders import load_json
 
 
 class RegexFactory:
-    def __init__(self, global_pattern, local_pattern):
-        self.globals = global_pattern["pattern"]
-        self.locals = local_pattern["pattern"].copy()
-        patterns = {}
-        patterns["week"] = "|".join(self.locals.get("week", []))
-        patterns["months"] = "|".join(self.locals.get("months", []))
-        self.locals["dates"] = [
-            date.format(**patterns) for date in self.locals["dates"]
-        ]
+    def __init__(self, lang):
+        self.globals = load_json("./data/lang/regex", "globals.json")
+        self.locals = load_json("./data/lang/regex", f"{lang}.json")
+        self.date_patterns = []
+        patterns = {
+            "week": "|".join(self.locals.get("week", [])),
+            "months": "|".join(self.locals.get("months", [])),
+        }
+        for date_format in self.locals["dates"]:
+            self.date_patterns.append(
+                date_format.replace("{week}", patterns["week"]).replace(
+                    "{months}", patterns["months"]
+                )
+            )
 
     def compile_rgx(self, *patterns):
         return re.compile("|".join(patterns), re.IGNORECASE)
@@ -81,83 +30,13 @@ class RegexFactory:
             "dificulty": self.compile_rgx(
                 self.globals["dificulty"], *self.locals.get("dificulty", [])
             ),
-            "date": self.compile_rgx(*self.locals["dates"]),
+            "date": self.compile_rgx(*self.date_patterns),
         }
 
 
-esp = RegexFactory(g_ui_and_patterns, esp_ui_and_patterns)
-
-test = "fecha es el proximo martes va a ser muy difícil, @pero es importante"
+esp = RegexFactory("es")
+test = "el 12 de noviembre martes va a ser muy difícil, @pero es importante"
 print(re.search(esp.get_regex()["date"], test))
 print(re.search(esp.get_regex()["important"], test))
 print(re.search(esp.get_regex()["project"], test))
 print(re.search(esp.get_regex()["dificulty"], test))
-
-# print(esp.globals)
-
-
-# class Global_str:
-#     _TASK_SEPARATOR = "//"
-#     _IMPORTANT = "\\*"
-#     _PROJECT = "@([a-zA-Z]+)"
-#     _DIFICULTY = "!([1-5])"
-#
-#     @classmethod
-#     def make_rgx(cls, *args):
-#         return re.compile("|".join(args), re.IGNORECASE)
-#
-#
-# class Esp_str(Global_str):
-#     __IMPORTANT_ALT = "importante"
-#     __DIFICULTY_LEVELS = ("(muy )?f[aá]cil", "normal", "(muy )?dif[ií]cil")
-#     __DAYS = (
-#         "lunes",
-#         "martes",
-#         "mi[eé]rcoles",
-#         "jueves",
-#         "viernes",
-#         "s[aá]bado",
-#         "domingo",
-#     )
-#     __MONTH_LIST = (
-#         "enero",
-#         "febrero",
-#         "marzo",
-#         "abril",
-#         "mayo",
-#         "junio",
-#         "julio",
-#         "agosto",
-#         "septiembre",
-#         "octubre",
-#         "noviembre",
-#         "diciembre",
-#     )
-#     __MONTH = "|".join(__MONTH_LIST)
-#     __WEEK = "|".join(__DAYS)
-#     __DIFICULTY_ALT = "|".join(__DIFICULTY_LEVELS)
-#     __DATE_1 = rf"([0-9]{{1,2}})(\/[0-9]{{1,2}}| de ({__MONTH}))"
-#     __DATE_2 = rf"(de (hoy|ma[nñ]ana|este ({__WEEK})) en |en )([0-9]+ )"
-#     __DATE_3 = r"(dentro de )([0-9]+ )(d[ií]as?|semanas?)"
-#     __DATE_4 = rf"(hoy|ma[nñ]ana)|(el |este |pr[oó]ximo )({__WEEK})"
-#
-#     @classmethod
-#     def importance_rgx(cls):
-#         return super().make_rgx(cls._IMPORTANT, cls.__IMPORTANT_ALT)
-#
-#     @classmethod
-#     def dificulty_rgx(cls):
-#         return super().make_rgx(cls._DIFICULTY, cls.__DIFICULTY_ALT)
-#
-#     @classmethod
-#     def project_rgx(cls):
-#         return super().make_rgx(cls._PROJECT)
-#
-#     @classmethod
-#     def date_rgx(cls):
-#         return super().make_rgx(
-#             cls.__DATE_1,
-#             cls.__DATE_2,
-#             cls.__DATE_3,
-#             cls.__DATE_4,
-#         )
