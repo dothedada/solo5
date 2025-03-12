f4e1557e00c22d0c6799fc08803a83ea833788a9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-03-12 12:05:34 -0500

    fixes gitignore

--- app/main.py
+++ app/main.py
@@ -8,14 +8,24 @@ def main():
     print(taskManager.heap)
 
     new_task = input("nuevas...\n")
-
     taskManager.add_tasks(new_task)
-    # new_tasks = []
-    # new_tasks.append(input("selecciona una tarea para borrar..."))
 
     search = input("que tareas???...\n")
     taskManager.search_by_task(search)
-    taskManager.update_task("done", True)
+    for i, task in taskManager.search_output():
+        print(f"{i}) {task.task}")
+
+    if len(taskManager.search_output()) > 9:
+        print(f"too many tasks by {search}")
+    elif len(taskManager.search_output()) == 1:
+        print("una")
+    else:
+        selection = input("select:\n")
+        taskManager.search_selection(selection)
+
+    nuevo = input("Ahora que???\n")
+    taskManager.update_task(nuevo)
+    taskManager.update_csv_from_heap()
 
     # print(taskManager.delete_task(new_tasks))
     print(taskManager.heap)
